a7e371d9cc85a36458857b53393779f66692771e
dingzhiwen <dingzw@ibaodashi.com>
2019-06-27 16:57:30 +0800

    fix archive

--- archive.py
+++ archive.py
@@ -10,8 +10,8 @@ from supports import print_split
 from supports import fir_upload
 from conf import config
 
-def main_archive(selectType, selectDebugType):
-    # selectDebugType 为 -1，则按照默认约定逻辑；为 0，则强制设置为 Dev 环境；为 1，则则强制设置为 Release 环境；
+def main_archive(selectType, cmdType):
+    # cmdType: -a:自动上传fir
     # 解析配置类型
     print_split.print_log('1.解析配置类型')
     packageType, netType = terminal_input.parser_select_type(selectType)
@@ -46,9 +46,10 @@ def main_archive(selectType, selectDebugType):
         status = status + '\n' + alertTitle
     # 拷贝到FIR
     if config.copy_ipa_to_fir :
-        print_split.print_log('8.上传到FIR')
-        fir_upload.upload_ipa(folderPath + exprotFileName + '.ipa', folderPath + iconpath, bundleid, netType, version, build)
-        status = status + '\n' + "上传FIR成功,请扫码下载"
+        if cmdType == 'a':
+            print_split.print_log('8.上传到FIR')
+            fir_upload.upload_ipa(folderPath + exprotFileName + '.ipa', folderPath + iconpath, bundleid, netType, version, build)
+            status = status + '\n' + "上传FIR成功,请扫码下载"
     # 发邮件
     if config.send_email :
         print_split.print_log('9.发邮件')
@@ -59,30 +60,30 @@ def main_archive(selectType, selectDebugType):
     # 结果弹窗
     print_split.print_log('10.结束')
     alert.show_detail_alert("自动打包结束", folderPath, showServicesPath)
-    
+
 def archive_ever_input(receiveInput):
     # 切换打包工程
     if code_update.safe_change_conf_python3(receiveInput.split(' ')) :
         return
-    # # 下载代码
-    # pod_tool.git_clone_repository()
-    # # 更新代码
-    # pod_tool.updateCode()
-    # # 下载Podfile中的代码
-    # pod_tool.installPods()
+    # 下载代码
+    pod_tool.git_clone_repository()
+    # 更新代码
+    pod_tool.updateCode()
+    # 下载Podfile中的代码
+    pod_tool.installPods()
     # 获取输入信息
     for s in receiveInput.split(' '):
         selectType = s
-        selectDebugType = -1
-        if len(selectType) == 3:
+        cmdType = ''
+        if len(selectType) >= 3:
             selectType = s[:1]
-            selectDebugType = s[2:]
+            cmdType = s[2:]
         # 恢复变化
         pod_tool.discardAllChange()
         # 清理缓存
         pod_tool.cleanProject()
         # 进入打包流程
-        main_archive(int(selectType), int(selectDebugType))
+        main_archive(int(selectType), cmdType)
 
 if __name__ == '__main__':
     receiveInput = terminal_input.receive_input()
